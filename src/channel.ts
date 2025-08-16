8b9d8b75b41a3e68a08879fefffda8b7290d93bd
EclipseWalker <phisanuratv100@hotmail.com>
2025-08-16 16:41:50 +0700

    fix: type and build

--- src/channel.ts
+++ src/channel.ts
@@ -2,22 +2,39 @@ import type { SyncMessage, TokenRecord } from './types';
 
 type OnMessage = (msg: SyncMessage) => void;
 
+type BCInstance = {
+  postMessage: (msg: any) => void;
+  close: () => void;
+  onmessage: ((e: { data: any }) => void) | null;
+};
+
 export class TokenChannel {
-  private bc?: BroadcastChannel;
+  private bc?: BCInstance;
   private name: string;
   private onMessage?: OnMessage;
 
   constructor(name: string) {
     this.name = name;
-    if (typeof BroadcastChannel !== 'undefined') {
-      this.bc = new BroadcastChannel(name);
-    } else {
-      window.addEventListener('storage', (e) => {
-        if (e.key === this.name && e.newValue) {
+
+    const g: any = (typeof globalThis !== 'undefined' ? globalThis : undefined) as any;
+
+    if (g && g.BroadcastChannel) {
+      this.bc = new g.BroadcastChannel(name) as BCInstance;
+      return;
+    }
+
+    const hasStorageEvents =
+      typeof globalThis !== 'undefined' &&
+      !!(globalThis as any).addEventListener &&
+      !!(globalThis as any).localStorage;
+
+    if (hasStorageEvents) {
+      (globalThis as any).addEventListener('storage', (e: any) => {
+        if (e?.key === this.name && e?.newValue) {
           try {
             const msg = JSON.parse(e.newValue) as SyncMessage;
             this.onMessage?.(msg);
-          } catch {}
+          } catch { }
         }
       });
     }
@@ -30,14 +47,14 @@ export class TokenChannel {
       try {
         localStorage.setItem(this.name, JSON.stringify(msg));
         localStorage.removeItem(this.name);
-      } catch {}
+      } catch { }
     }
   }
 
   listen(cb: OnMessage) {
     this.onMessage = cb;
     if (this.bc) {
-      this.bc.onmessage = (e) => cb(e.data as SyncMessage);
+      this.bc.onmessage = (e: any) => cb(e.data as SyncMessage);
     }
     return () => {
       if (this.bc) this.bc.close();
